d9ac29ad6223c4c960bbac1b3c914c6cfe69e277
linbudu <linbudu599@gmail.com>
2022-06-17 16:56:49 +0800

    feat: seed

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,20 @@ import { ArticleController } from './controllers/article.controller';
 import { TagController } from './controllers/tag.controller';
 import { CategoryController } from './controllers/category.controller';
 
+import { SeedController } from './controllers/seed.controller';
+
 import { ArticleService } from './services/article.service';
 import { TagService } from './services/tag.service';
 import { CategoryService } from './services/catagory.service';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [ArticleController, TagController, CategoryController],
+  controllers: [
+    ArticleController,
+    TagController,
+    CategoryController,
+    SeedController,
+  ],
   providers: [ArticleService, CategoryService, TagService],
 })
 export class AppModule {}
